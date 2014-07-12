2025bf708883083b2b28ddad9f5e5d938593cc04
Sanjib <sanjib@hotmail.com>
2014-07-12 23:49:47 +0600

    print — Output a string

---
+++ print..php
@@ -0,0 +1,19 @@
+<?php
+print("Hello World");
+
+print "print() also works without parentheses.";
+
+print "This spans
+multiple lines. The newlines will be
+output as well";
+
+print "This spans\nmultiple lines. The newlines will be\noutput as well.";
+
+print "escaping characters is done \"Like this\".";
+
+// You can use variables inside a print statement
+$foo = "foobar";
+$bar = "barbaz";
+
+print "foo is $foo"; // foo is foobar
+?>
